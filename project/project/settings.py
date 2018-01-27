ed3ad1ada0b0baf75f539a94c3d2959bf86eee5a
zikner <mitchyjeffy@gmail.com>
2018-01-27 16:19:16 +0300

    configured postgresql
    set up the project to work with postgresql and did the initial migration.

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'e_lab',
+        'USER': 'krypto',
+        'PASSWORD': 'i@mw3ffy',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
